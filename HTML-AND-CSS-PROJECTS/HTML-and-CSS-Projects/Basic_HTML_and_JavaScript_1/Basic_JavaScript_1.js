a93cdf3f67416ebc7d4682069621407b1afaf23e
JakeGonzalez95 <144293898+JakeGonzalez95@users.noreply.github.com>
2024-01-28 15:55:46 -0700

    Dwqd

---
+++ HTML-AND-CSS-PROJECTS/HTML-and-CSS-Projects/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Hello, World! I am an alert in a box!");
+ <script>
+    function My_First_Function()  {
+      var string = "Kiss me, My Lover!";
+      var result = String.fontcolor("green");
+      document.getElementById("Lea").innerHTML =
+      result;
+  }
+  </script>
